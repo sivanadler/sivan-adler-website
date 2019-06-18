9fc601cd93eeca84e3e421df2adda5978caf579d
Your Name <sivanadler@users.noreply.github.com>
2019-06-18 09:52:27 -0400

    trying to clear input on click, but react-mail-form makes it tricky

--- src/Contact.js
+++ src/Contact.js
@@ -5,20 +5,27 @@ import ReactContactForm from 'react-mail-form';
 
 class Contact extends React.Component {
 
+  clearInput = () => {
+    debugger
+    //need to figure out how to clear inputs after clicking send
+  }
+
   render() {
     return (
       <div>
         <Nav history={this.props.history}/>
         <h1 className="header">CONTACT</h1>
 
-        <ReactContactForm
-          titlePlaceholder="Subject"
-          contentsPlaceholder="Message..."
-          buttonText="Send"
-          className="contact-form"
-          to="sadler2007@gmail.com"
-          onClick={() => console.log("hey")}
-        />
+        <div className="contact-form">
+          <p className="contact-sub-header">To get in touch, please send me an email and I will get back to you as soon as possible: </p>
+          <ReactContactForm
+            titlePlaceholder="Subject"
+            contentsPlaceholder="Message..."
+            buttonText="Send"
+            to="sadler2007@gmail.com"
+            onClick={this.clearInput}
+          />
+        </div>
 
       </div>
     )
