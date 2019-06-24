65b00a05c58eb1512b46838f2601f36576bc4045
Your Name <sivanadler@users.noreply.github.com>
2019-06-24 17:58:51 -0400

    videos embeded but need to be styled

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -25,24 +25,39 @@ class ProjectCard extends React.Component {
   render() {
     return (
       <div className='project-card'>
-        <a className='project-card-header' href={this.props.link}>
-          <h1 className='project-card-header'>{this.props.name}</h1>
-        </a>
-        <p>{this.props.description}</p>
+      <div className='text-div'>
+          <a className='project-card-header' href={this.props.link}>
+            <h1 className='project-card-header'>{this.props.name}</h1>
+          </a>
+          <p>{this.props.description}</p>
 
-        <br />
+          <br />
 
-        <p>TECHNOLOGIES:</p>
-        <ul>
-          {this.getFrontEndTechnologies()}
-        </ul>
+          <p className='project-card-sub-header'>TECHNOLOGIES:</p>
+          <ul>
+            {this.getFrontEndTechnologies()}
+          </ul>
 
-        <br />
+          <br />
 
-        <p>FUNCTIONALITY HIGHLIGHTS:</p>
-        <ul>
-          {this.getAccomplishments()}
-        </ul>
+          <p className='project-card-sub-header'>FUNCTIONALITY HIGHLIGHTS:</p>
+          <ul>
+            {this.getAccomplishments()}
+          </ul>
+        </div>
+        <div className="video" >
+          <iframe
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "90%"
+            }}
+            src={`https://www.youtube.com/embed/${this.props.demo}`}
+            frameBorder="0"
+          />
+        </div>
       </div>
     )
   }
