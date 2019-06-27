8ec0f587b8d58c9d4a69f8ef1261874567386756
Your Name <sivanadler@users.noreply.github.com>
2019-06-27 08:45:03 -0400

    contact page styling

--- src/Contact.js
+++ src/Contact.js
@@ -13,20 +13,23 @@ class Contact extends React.Component {
   render() {
     return (
       <div>
-        <Nav history={this.props.history}/>
-        <h1 className="header">CONTACT</h1>
-
-        <div className="contact-form">
-          <p className="contact-sub-header">To get in touch, please send me an email and I will get back to you as soon as possible: </p>
-          <ReactContactForm
-            titlePlaceholder="Subject"
-            contentsPlaceholder="Message..."
-            buttonText="Send"
-            to="sadler2007@gmail.com"
-            onClick={this.clearInput}
-          />
+        <div className="left">
+          <Nav history={this.props.history}/>
         </div>
 
+        <div className="right">
+          <div className="contact-form">
+            <h1 className="header">CONTACT</h1>
+            <p className="contact-sub-header">To get in touch, please send me an email and I will get back to you as soon as possible: </p>
+            <ReactContactForm
+              titlePlaceholder="Subject"
+              contentsPlaceholder="Message..."
+              buttonText="Send"
+              to="sadler2007@gmail.com"
+              onClick={this.clearInput}
+            />
+          </div>
+        </div>
       </div>
     )
   }
