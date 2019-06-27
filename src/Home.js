15d0760956f6062d05c1f921a115ea5607bfbe82
Your Name <sivanadler@users.noreply.github.com>
2019-06-27 14:59:18 -0400

    styling home page and added social links

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import linkedin from './icons/linkedin.png'
+import wordpress from './icons/wordpress.png'
+import github from './icons/github.png'
 
 class Home extends React.Component {
 
@@ -6,7 +9,13 @@ class Home extends React.Component {
     return (
       <div className="home-page-div">
         <h1 className="header">SIVAN ADLER</h1>
-        <h3 className="sub-header">Full Stack Software Engineer</h3>
+        <h3 className="sub-header">Full Stack Software Engineer with An Eye For Design</h3>
+        <p className="interests"> NYC Native <span className='seperator'>|</span> Israeli  <span className='seperator'>|</span> Foodie <span className='seperator'>|</span> Dog Lover <span className='seperator'>|</span> Studio Artist </p>
+        <div className="socials">
+          <a href="https://www.linkedin.com/in/sivanadler"><img className="social-icons" src={linkedin} /></a>
+          <a href="https://github.com/sivanadler"><img className="social-icons" src={github} /></a>
+          <a href="https://sivanadler.wordpress.com/"><img className="social-icons" src={wordpress} /></a>
+        </div>
       </div>
     )
   }
--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import { Redirect } from 'react-router'
 import sivan from './images/sivan-adler.jpg'
+import linkedin from './icons/linkedin.png'
+import wordpress from './icons/wordpress.png'
+import github from './icons/github.png'
 
 class Nav extends React.Component {
 
@@ -32,6 +35,7 @@ class Nav extends React.Component {
           <NavLink to="/about" exact className="main-nav-link"><p className="main-nav-link-text" onClick={this.handleAboutMe}><strong>ABOUT ME</strong></p></NavLink>
           <NavLink to="/projects" exact className="main-nav-link"><p className="main-nav-link-text" onClick={this.handleProjects}><strong>PROJECTS</strong></p></NavLink>
           <NavLink to="/contact" exact className="main-nav-link"><p className="main-nav-link-text" onClick={this.handleContact}><strong>CONTACT</strong></p></NavLink>
+          <br/>
         </Router>
       </div>
     )
