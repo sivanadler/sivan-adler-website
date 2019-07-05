cc68a65fe70544976a9313c8404f457f2e371511
Your Name <sivanadler@users.noreply.github.com>
2019-07-05 14:20:46 -0400

    home page styling for deployment

--- src/Home.js
+++ src/Home.js
@@ -6,14 +6,12 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="home-page-div">
-          <h1 className="header">SIVAN ADLER</h1>
-          <h3 className="sub-header">Full Stack Software Engineer with An Eye For Design</h3>
-          <p className="interests"> NYC Native <span className='seperator'>|</span> Israeli  <span className='seperator'>|</span> Foodie <span className='seperator'>|</span> Dog Lover <span className='seperator'>|</span> Studio Artist </p>
-          <div className="socials">
+      <div className="home-page-div">
+        <h1 className="header">SIVAN ADLER</h1>
+        <h3 className="sub-header">Full Stack Software Engineer with An Eye For Design</h3>
+        <p className="interests"> NYC Native <span className='seperator'>|</span> Israeli  <span className='seperator'>|</span> Foodie <span className='seperator'>|</span> Dog Lover <span className='seperator'>|</span> Studio Artist </p>
+        <div className="socials">
 
-          </div>
         </div>
       </div>
     )
