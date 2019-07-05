f19d16037133aee9016be10d803dd145c2c7e5a3
Your Name <sivanadler@users.noreply.github.com>
2019-07-05 15:38:18 -0400

    testing socials again

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from 'react-social-icons';
 import Nav from './Nav'
 
 class Home extends React.Component {
@@ -11,33 +11,30 @@ class Home extends React.Component {
         <h3 className="sub-header">Full Stack Software Engineer with An Eye For Design</h3>
         <p className="interests"> NYC Native <span className='seperator'>|</span> Israeli  <span className='seperator'>|</span> Foodie <span className='seperator'>|</span> Dog Lover <span className='seperator'>|</span> Studio Artist </p>
         <div className="socials">
-
+          <SocialIcon
+            url="http://linkedin.com/in/sivanadler"
+            bgColor="currentColor"
+            fgColor="white"
+            className="social-icons"
+            style={{ height: 75, width: 75 }}
+          />
+          <SocialIcon
+            url="http://github.com/sivanadler"
+            bgColor="currentColor"
+            fgColor="white"
+            className="social-icons"
+            style={{ height: 75, width: 75 }}
+          />
+          <SocialIcon
+            url="https://sivanadler.wixsite.com/sivanportfolio"
+            bgColor="currentColor"
+            fgColor="white"
+            className="social-icons"
+            style={{ height: 75, width: 75 }}
+          />
         </div>
       </div>
     )
   }
 }
 export default Home
-
-
-// <SocialIcon
-//   url="http://linkedin.com/in/sivanadler"
-//   bgColor="currentColor"
-//   fgColor="white"
-//   className="social-icons"
-//   style={{ height: 75, width: 75 }}
-// />
-// <SocialIcon
-//   url="http://github.com/sivanadler"
-//   bgColor="currentColor"
-//   fgColor="white"
-//   className="social-icons"
-//   style={{ height: 75, width: 75 }}
-// />
-// <SocialIcon
-//   url="https://sivanadler.wixsite.com/sivanportfolio"
-//   bgColor="currentColor"
-//   fgColor="white"
-//   className="social-icons"
-//   style={{ height: 75, width: 75 }}
-// />
