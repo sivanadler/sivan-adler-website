1da856f66dca88381873fcff023ad8e37c8c1b2c
Your Name <sivanadler@users.noreply.github.com>
2019-07-14 22:16:14 -0400

    more skills and hover text

--- src/Nav.js
+++ src/Nav.js
@@ -50,7 +50,7 @@ class Nav extends React.Component {
               <span className='hamburger'></span>
               <span className='hamburger'></span>
               <span className='hamburger'></span>
-              <p className="mobile-text">SIVAN ADLER: SOFTWARE ENGINEER</p>
+              <p className="mobile-text"><strong>SIVAN ADLER:</strong> SOFTWARE ENGINEER</p>
             <ul id="menu">
               <NavLink to="/" exact className='logo-nav-link'> <img className="logo" src={logo}/> </NavLink>
               <NavLink to="/" exact className={this.getclassName("/")} onClick={this.handleGoHome}><p className="main-nav-link-text"><strong>HOME</strong></p></NavLink>
